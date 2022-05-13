c6fe03502947092cbc710b3830ff1ee212d484f3
David14gb <davidgarciab14@gmail.com>
2022-05-13 14:08:53 +0200

    Reto 9 Pdf2

--- OOPReview/pdf2.ts
+++ OOPReview/pdf2.ts
@@ -1,12 +1,12 @@
 export class Mobile{
-    public name:string
-    public model:string
-    public trademark:string
-    public sdSize:string
-    public color:string
-    public is5G:boolean
-    public cameraNumber:number
-    public price:number
+    private name:string
+    private model:string
+    private trademark:string
+    private sdSize:string
+    private color:string
+    private is5G:boolean
+    private cameraNumber:number
+    private price:number
     
     constructor(name:string, model:string, trademark:string, sdSize:string, color:string, is5G:boolean, cameraNumber:number, price:number){
         this.name = name
@@ -21,5 +21,60 @@ export class Mobile{
     printAll():void{
         console.log(this.name + "\n" + this.model + "\n" + this.trademark + "\n" + 
         this.sdSize + "\n" + this.color + "\n" + this.is5G + "\n" + this.cameraNumber + "\n" + this.price + "\n")
+    }
+    public getName():string{
+        return this.name
+    }
+    public getModel():string{
+        return this.model
+    }
+    public getTrademark():string{
+        return this.trademark
+    }
+    public getSdSize():string{
+        return this.sdSize
+    }
+    public getColor():string{
+        return this.color
+    }
+    public getIs5G():boolean{
+        return this.is5G
+    }
+    public getCameraNumber():number{
+        return this.cameraNumber
+    }
+    public getPrice():number{
+        return this.price
+    }
+    public setName(name:string){
+        this.name = name
+    }
+    public setModel(model:string){
+        this.model = model
+    }
+    public setTrademark(trademark:string){
+        this.trademark = trademark
+    }
+    public setSdSize(sdSize:string){
+        this.sdSize = sdSize
+    }
+    public setColor(color:string){
+        this.color = color
+    }
+    public setIs5G(is5G:boolean){
+        this.is5G = is5G
+    }
+    public setCameraNumber(cameraNumber:number){
+        this.cameraNumber = cameraNumber
+    }
+    public setPrice(price:number){
+        this.price = price
+    }
+    printCarac(){
+        console.log("The Characteristics of the mobile " + this.name + " are:" + "\n" +
+        "Name: " + this.name + "\n" + "Model: " + this.model + "\n" + "Trademark" + this.trademark + "\n" +
+        "SD Size (GB): " + this.sdSize + "\n" + "Color: " + this.color + "\n" 
+        + "Is 5g: " + this.is5G + + "\n" + "\n" + "Number of Cameras: " + this.cameraNumber + "\n");
+        
     }
 }
--- OOPReview/pdfN.ts
+++ OOPReview/pdfN.ts
@@ -4,15 +4,20 @@ let nokia3219 = new Mobile("Nokia","3210","Nokia", "No tiene", "Azul", false, 0,
 let iPhone3G  = new Mobile("iPhone", "3G", "Apple", "8GB", "Negro", false, 1, 100);
 let samsungGalaxy10 = new Mobile("Samsung", "Galaxy 10", "Samsung", "512GB", "Blanco", true, 4, 485);
 
-console.log(nokia3219);
 nokia3219.printAll();
-console.log(iPhone3G);
 iPhone3G.printAll();
-console.log(samsungGalaxy10);
 samsungGalaxy10.printAll();
-// nokia3219.is5G = true
-// nokia3219.cameraNumber = 4
+
+nokia3219.setIs5G(true);
+nokia3219.setCameraNumber(4);
+
 nokia3219.printAll();
 nokia3219.printCarac();
-iPhone3G.printCarac();
-samsungGalaxy10.printCarac();
+
+let ar: Mobile[] = []
+ar.push(nokia3219, iPhone3G, samsungGalaxy10);
+for(let i = 0; i < ar.length; i++){
+    ar[i].printCarac();
+    
+}
+
